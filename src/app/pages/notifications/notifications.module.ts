8f33ab89d05213fb830008522ff5306340121930
unknown <adiontae@spxmigration.com>
2019-11-23 14:09:44 -0600

    fsafsa

--- src/app/pages/notifications/notifications.module.ts
+++ src/app/pages/notifications/notifications.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { NotificationsPage } from './notifications.page';
+import { SharedModule } from 'src/app/modules/shared/shared.module';
 
 const routes: Routes = [
   {
@@ -18,6 +19,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     FormsModule,
+    SharedModule,
     IonicModule,
     RouterModule.forChild(routes)
   ],
